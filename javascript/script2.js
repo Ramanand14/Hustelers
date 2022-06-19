4753de4deae3c6d3c74c60e8f779f4c527fd6820
Ramanand Anand Mohare <ramanandmohare14@gmail.com>
2022-06-19 13:19:30 +0530

    Delete javascript directory

--- javascript/script2.js
+++
@@ -1,5 +0,0 @@
-const div = document.querySelector('.box1');
-
-div.addEventListener('click', (event) => {
-  window.open('../html/details.html')
-});
--- javascript/script3.js
+++
@@ -1,3 +0,0 @@
-var vOneLS = localStorage.getItem("vOneLocalStorage");  
-var variableTwo = vOneLS;  
-console.log(variableTwo)
